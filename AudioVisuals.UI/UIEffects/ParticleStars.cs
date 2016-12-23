4f84a0c4292919ef78836993174a85b42f6d9cf4
Jon Halliday <jhalliday@gmail.com>
2016-12-23 12:52:11 -0500

    Field of view changed from 45 to 60 degrees. Minor tweaks.

--- AudioVisuals.UI/UIEffects/ParticleStars.cs
+++ AudioVisuals.UI/UIEffects/ParticleStars.cs
@@ -13,7 +13,7 @@ namespace AudioVisuals.UI
         private const float HalfPi = (float)Math.PI / 2.0f;
         private const float PIPart = HalfPi / BandCount;
         private const float TotalWidth = 100.0f;
-        private const float ExlcusionFromOriginRadius = 30.0f;
+        private const float ExlcusionFromOriginRadius = 32.0f;
 
         #endregion
 
